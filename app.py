0f9022bf8a0b3f734ec84bd54dbdc54fef0e1cfe
matheussacg <matheus.sacg@gmail.com>
2024-09-16 10:59:00 -0300

    fix: Chama as funções de estado e renderização na ordem correta e remove a mensagem de boas vindas da página principal

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+
 st.set_page_config(page_title="Home Page", page_icon="🏡")
 from auth.state import get_state
 from auth.menu import render_menu
@@ -6,12 +7,10 @@ from utils.styles import remove_st_decoration
 
 remove_st_decoration()
 
+render_menu()
+
 state = get_state()
+
 if state.authenticated:
     st.title("This page is available to all users")
     st.markdown(f"You are currently logged with the role of {state.role}.")
-else:
-    st.title("Welcome")
-    st.markdown("Please log in to access the application.")
-
-render_menu()
